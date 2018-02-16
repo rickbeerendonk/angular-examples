e625c27ac9462162f78ecc61876c5659f20f91d6
Rick Beerendonk <rick@beerendonk.com>
2018-02-16 01:25:39 +0100

    Add Decimal Pipe example.

---
+++ 04 - Pipes/1. Build-in/e. Decimal/app/greeting.component.ts
@@ -0,0 +1,39 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+import { Component } from '@angular/core';
+
+/*
+To use other locales than the default en-US, 
+call registerLocaleData(): see app.module.ts 
+
+Documentation: https://angular.io/guide/i18n#setting-up-locale
+*/
+
+@Component({
+  selector: 'greeting',
+  template: `
+  <p>
+    {{123.456 | number}}
+    <strong> = Default</strong>
+  </p>
+  <p>
+    {{123.456 | number:'1.0-1'}}
+    <strong> = Min. 1 integer digit & max. 1 digit after fraction</strong>
+  </p>
+  <p>
+    {{123.456 | number:'5.5-5'}}
+    <strong> = Min. 5 integer digits & min. 5 digits after fraction</strong>
+  </p>
+  <p>
+    {{1234.567 | number:'':'en'}}
+    <strong> = English</strong>
+  </p>
+  <p>
+    {{1234.567 | number:'':'fr'}}
+    <strong> = French</strong>
+  </p>
+`
+})
+export class GreetingComponent {
+}
