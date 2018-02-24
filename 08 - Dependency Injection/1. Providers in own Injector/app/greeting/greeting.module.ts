71dc585e20c661e8a5a631217c56741057c8a836
Rick Beerendonk <rick@beerendonk.com>
2018-02-24 11:48:27 +0100

    Correction.

--- 08 - Dependency Injection/1. Providers in own Injector/app/greeting/greeting.module.ts
+++ 08 - Dependency Injection/1. Providers in own Injector/app/greeting/greeting.module.ts
@@ -5,12 +5,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GreetingComponent } from './greeting.component';
-import { LoggerService } from '../services/logger.service';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [GreetingComponent],
   exports: [GreetingComponent],
-  providers: [LoggerService],
 })
 export class GreetingModule { }
