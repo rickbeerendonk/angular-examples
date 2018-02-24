fe19b8455e47a1e17173d5eaadaf7ce9963bb1c1
Rick Beerendonk <rick@beerendonk.com>
2018-02-24 11:40:03 +0100

    Add Dependency Injection - Provider in own Injector example.

---
+++ 08 - Dependency Injection/1. Providers in own Injector/app/greeting/greeting.module.ts
@@ -0,0 +1,16 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GreetingComponent } from './greeting.component';
+import { LoggerService } from '../services/logger.service';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [GreetingComponent],
+  exports: [GreetingComponent],
+  providers: [LoggerService],
+})
+export class GreetingModule { }
