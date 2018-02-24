b087db2fa733413046ab713cdb3b461ba711dccc
Rick Beerendonk <rick@beerendonk.com>
2018-02-24 12:01:29 +0100

    Add Dependency Injection - Providers in Application Module example.

---
+++ 08 - Dependency Injection/3. Providers in Application Module/app/app.component.ts
@@ -0,0 +1,16 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+import { Component } from '@angular/core';
+
+import { LoggerService } from './services/logger.service';
+
+@Component({
+  selector: 'app',
+  template: '<greeting></greeting>',
+})
+export class AppComponent {
+  constructor(logger: LoggerService) {
+    logger.info('AppComponent created.');
+  }
+}
---
+++ 08 - Dependency Injection/3. Providers in Application Module/app/greeting/greeting.component.ts
@@ -0,0 +1,16 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+import { Component } from '@angular/core';
+
+import { LoggerService } from '../services/logger.service';
+
+@Component({
+  selector: 'greeting',
+  template: '<h1>Hello World</h1>',
+})
+export class GreetingComponent {
+  constructor(logger: LoggerService) {
+    logger.warn('GreetingComponent created.');
+  }
+}
