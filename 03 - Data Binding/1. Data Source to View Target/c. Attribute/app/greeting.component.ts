f8a9de0948cc92c25519b99cd52d57e3fc6c29ec
Rick Beerendonk <rick@beerendonk.com>
2018-02-25 20:04:40 +0100

    Add Data Binding - Attribute example.

---
+++ 03 - Data Binding/1. Data Source to View Target/c. Attribute/app/greeting.component.ts
@@ -0,0 +1,15 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'greeting',
+  template: `
+    <h1 [attr.aria-label]="name">
+      Hello World!
+    </h1>`
+})
+export class GreetingComponent {
+  name: string = 'World';
+}
