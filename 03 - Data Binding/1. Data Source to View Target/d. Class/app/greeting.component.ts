c4d6859c670aad71b1df1e951d5202da158e4809
Rick Beerendonk <rick@beerendonk.com>
2018-02-25 20:26:40 +0100

    Add Data Binding - Class example.

---
+++ 03 - Data Binding/1. Data Source to View Target/d. Class/app/greeting.component.ts
@@ -0,0 +1,16 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'greeting',
+  template: `
+    <h1 [class.isImportant]="important">
+      Hello World!
+    </h1>`,
+  styles: [".isImportant { text-decoration: underline; }"]
+})
+export class GreetingComponent {
+  important: boolean = true;
+}
