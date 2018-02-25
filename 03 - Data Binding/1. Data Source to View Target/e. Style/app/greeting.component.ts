dbff7f7592572df4fa5001a0bec8f2fd8bdf8986
Rick Beerendonk <rick@beerendonk.com>
2018-02-25 20:41:20 +0100

    Add Data Binding - Style example.

---
+++ 03 - Data Binding/1. Data Source to View Target/e. Style/app/greeting.component.ts
@@ -0,0 +1,15 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'greeting',
+  template: `
+    <h1 [style.color]="important ? 'orange' : 'gray'">
+      Hello World!
+    </h1>`
+})
+export class GreetingComponent {
+  important: boolean = true;
+}
