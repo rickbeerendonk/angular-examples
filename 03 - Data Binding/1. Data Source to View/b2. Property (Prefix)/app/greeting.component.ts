1ed2198222ea7d1850cf775304dcb24e2b5526eb
Rick Beerendonk <rick@beerendonk.com>
2018-02-25 01:45:28 +0100

    Add Data Binding as Property examples.

---
+++ 03 - Data Binding/1. Data Source to View/b2. Property (Prefix)/app/greeting.component.ts
@@ -0,0 +1,15 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'greeting',
+  template: `
+    <h1>
+      Hello <span bind-innerText='name'></span>!
+    </h1>`
+})
+export class GreetingComponent {
+  name: string = 'World';
+}
