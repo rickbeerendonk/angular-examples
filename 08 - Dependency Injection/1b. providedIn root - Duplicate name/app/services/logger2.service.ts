4f3845fdbf66097724e3d5d58b9c24f4bf723f69
Rick Beerendonk <rick@beerendonk.com>
2018-11-01 06:39:02 +0100

    Add "Dependency Injection - providedIn root - Duplicate name" example.

---
+++ 08 - Dependency Injection/1b. providedIn root - Duplicate name/app/services/logger2.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoggerService {
+  private static uniqueId: number = 0;
+
+  private id: number;
+
+  constructor() {
+    this.id = ++LoggerService.uniqueId;
+  }
+
+  error(message?: any, ...optionalParams: any[]) {
+    console.error(message, ...optionalParams, `[logger2: ${this.id}]`);
+  }
+
+  info(message?: any, ...optionalParams: any[]) {
+    console.info(message, ...optionalParams, `[logger2: ${this.id}]`);
+  }
+
+  warn(message?: any, ...optionalParams: any[]) {
+    console.warn(message, ...optionalParams, `[logger2: ${this.id}]`);
+  }
+}
