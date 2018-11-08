66a55bd4be70a92d2f92a593f7f90dd2f73bfe4e
Rick Beerendonk <rick@beerendonk.com>
2018-11-08 07:56:29 +0100

    Add "Pipes - Build-in - Date" example.

---
+++ 04 - Pipes/1. Build-in/h. Date/app/app.module.ts
@@ -0,0 +1,23 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeNl from '@angular/common/locales/nl';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { DemoComponent } from './demo.component';
+
+registerLocaleData(localeNl, 'nl');
+
+@NgModule({
+  providers: [
+    /* Dutch is the default now instead of en-US */
+    { provide: LOCALE_ID, useValue: "nl-NL" }
+  ],
+  imports: [BrowserModule],
+  declarations: [DemoComponent],
+  bootstrap: [DemoComponent]
+})
+export class AppModule { }
---
+++ 04 - Pipes/1. Build-in/h. Date/app/demo.component.ts
@@ -0,0 +1,44 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+import { Component } from '@angular/core';
+
+/*
+To use other locales than the default en-US, 
+call registerLocaleData(): see app.module.ts 
+
+Documentation: https://angular.io/guide/i18n#setting-up-locale
+*/
+
+@Component({
+  selector: 'demo',
+  template: `
+  <p>
+    {{date | date}}
+    <strong> = Default</strong>
+  </p>
+  <p>
+    {{date | date:'full'}}
+    <strong> = Format: 'full'</strong>
+  </p>
+  <p>
+    {{date | date:'mm ss'}}
+    <strong> = Format: 'mm ss'</strong>
+  </p>
+  <p>
+    {{date | date:'short':'+0430'}}
+    <strong> = Timezone +0430 (short format)</strong>
+  </p>
+  <p>
+    {{date | date:'full':'':'en-US'}}
+    <strong> = English</strong>
+  </p>
+  <p>
+    {{date | date:'full':'':'nl'}}
+    <strong> = Dutch</strong>
+  </p>
+`
+})
+export class DemoComponent {
+  date: Date = new Date();
+}
