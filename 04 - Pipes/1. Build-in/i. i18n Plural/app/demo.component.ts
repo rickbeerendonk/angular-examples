dacbf26cb73e314f460aaaa8e181b1e71b149fdf
Rick Beerendonk <rick@beerendonk.com>
2018-11-08 09:16:56 +0100

    Add "Pipes - Build-in - i18n Plural" example.

---
+++ 04 - Pipes/1. Build-in/i. i18n Plural/app/demo.component.ts
@@ -0,0 +1,30 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+import { Component } from '@angular/core';
+
+/*
+To use other locales than the default en-US, 
+call registerLocaleData(): see app.module.ts 
+
+Documentation: https://angular.io/guide/i18n#setting-up-locale
+*/
+
+@Component({
+  selector: 'demo',
+  template: `
+  <p>
+    {{ 0 | i18nPlural:numberMap }}
+    <strong> = Default</strong>
+    <br>
+    {{ 1 | i18nPlural:numberMap }}
+    <strong> = Default</strong>
+    <br>
+    {{ 9 | i18nPlural:numberMap }}
+    <strong> = Default</strong>
+  </p>
+  `
+})
+export class DemoComponent {
+  numberMap: {[count: string]: string} = {'=0': 'No items.', '=1': 'One item.', 'other': '# items.'};
+}
