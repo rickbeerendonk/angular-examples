8b13887769d57f9571e076473c13d387d16d7aeb
Rick Beerendonk <rick@beerendonk.com>
2018-11-08 10:00:06 +0100

    Add "Pipes - Build-in - i18n Select" example.

---
+++ 04 - Pipes/1. Build-in/j. i18n Select/app/app.module.ts
@@ -0,0 +1,18 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+import { registerLocaleData } from '@angular/common';
+import localeNl from '@angular/common/locales/nl';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { DemoComponent } from './demo.component';
+
+registerLocaleData(localeNl, 'nl');
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [DemoComponent],
+  bootstrap: [DemoComponent]
+})
+export class AppModule { }
---
+++ 04 - Pipes/1. Build-in/j. i18n Select/app/demo.component.ts
@@ -0,0 +1,33 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+import { Component } from '@angular/core';
+
+/*
+To use other locales than the default en-US, 
+call registerLocaleData(): see app.module.ts 
+
+Documentation: https://angular.io/guide/i18n#setting-up-locale
+*/
+
+@Component({
+  selector: 'demo',
+  template: `
+  <p>
+    {{ 'US' | i18nSelect:countryMap }}
+    <br>
+    {{ 'NO' | i18nSelect:countryMap }}
+    <br>
+    {{ 'NL' | i18nSelect:countryMap }}
+    <br>
+    {{ 'CO' | i18nSelect:countryMap }}
+  </p>
+  `
+})
+export class DemoComponent {
+  countryMap: Object = {
+    'NL': 'The Netherlands', 
+    'NO': 'Norway', 
+    'US': 'United States',
+    'other': '(unknown)'};
+}
