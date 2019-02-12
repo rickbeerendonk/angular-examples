3ec618d09246a3bb9c9972d9d4d3300fa1f99af7
Rick Beerendonk <rick@beerendonk.com>
2019-02-12 17:29:43 -0500

    Add "Router - No Match" example.

---
+++ 14 - Router/2. No Match/app/page2/page2.component.ts
@@ -0,0 +1,11 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: __moduleName /* Use __moduleName in SystemJS and module.id in commonjs */,
+  selector: 'page2',
+  templateUrl: './page2.component.html'
+})
+export class Page2Component {}
