25c4a210e03b2e22cfa2f6fc4722f6be2bcdc8e0
Rick Beerendonk <rick@beerendonk.com>
2019-02-17 17:19:26 -0500

    Add "Data Binding with Templates - NgForOf - For with Even Odd (extended)" example.

---
+++ 04 - Data Binding with Templates/2. NgForOf/d1. For with Odd Even (extended)/app/app.component.ts
@@ -0,0 +1,13 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: __moduleName /* Use __moduleName in SystemJS and module.id in commonjs */,
+  selector: 'app',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  items: Array<string> = ['one', 'two', 'three', 'four'];
+}
