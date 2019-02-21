a7d45c42dbbfd96cb66408f9b3a7e7292ea2a30b
Rick Beerendonk <rick@beerendonk.com>
2019-02-21 09:37:20 -0500

    Add "Router - Redirect" example.

---
+++ 15 - Router/3. Redirect/default/app/app.module.ts
@@ -0,0 +1,26 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'page1', component: Page1Component },
+      { path: 'page2', component: Page2Component },
+      { path: 'oldpage2', redirectTo: 'page2', pathMatch: 'full' }
+    ])
+  ],
+  declarations: [AppComponent, HomeComponent, Page1Component, Page2Component],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
