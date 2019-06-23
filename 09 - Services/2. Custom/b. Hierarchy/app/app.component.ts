d19dca87e0706a774e243c9224abd4bef386181a
Rick Beerendonk <rick@beerendonk.com>
2019-06-23 08:31:47 +0200

    Add "Services - Custom - Hierarchy" example.

---
+++ 09 - Services/2. Custom/b. Hierarchy/app/app.component.ts
@@ -0,0 +1,19 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+
+import { LoggerService } from './logger/logger.service';
+
+@Component({
+  selector: 'app',
+  template: `
+    <greeting>Should be replaced.</greeting>
+  `,
+  providers: [LoggerService]
+})
+export class AppComponent {
+  constructor(private loggerService: LoggerService) {
+    loggerService.log('AppComponent created.');
+  }
+}
---
+++ 09 - Services/2. Custom/b. Hierarchy/app/greeting/greeting.module.ts
@@ -0,0 +1,20 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GreetingComponent } from './greeting.component';
+import { LoggerService } from '../logger/logger.service';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [GreetingComponent],
+  exports: [GreetingComponent],
+  providers: [LoggerService]
+})
+export class GreetingModule {
+  constructor(private loggerService: LoggerService) {
+    loggerService.log('GreetingModule created.');
+  }
+}
