4b645b80e730d2641de42677c3c5776f9b43668e
Rick Beerendonk <rick@beerendonk.com>
2019-06-23 20:52:50 +0200

    Component - Styles - Relative URL - Multiple

---
+++ 02 - Component - c. Styles/3. Relative URL - a. Multiple/app/app.component.ts
@@ -0,0 +1,13 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  template: `
+    <div>App component</div>
+    <greeting></greeting>
+  `
+})
+export class AppComponent {}
---
+++ 02 - Component - c. Styles/3. Relative URL - a. Multiple/app/greeting.component.ts
@@ -0,0 +1,16 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: __moduleName /* Use __moduleName in SystemJS and module.id in commonjs */,
+  selector: 'greeting',
+  styleUrls: ['./greeting.component.css', './second.greeting.component.css'],
+  template: `
+    <div>Hello World</div>
+    <span>I am green</span>
+    <child></child>
+  `
+})
+export class GreetingComponent {}
