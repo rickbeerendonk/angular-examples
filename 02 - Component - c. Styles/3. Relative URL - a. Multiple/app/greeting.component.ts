582fd61117343c147dc9615b16e1682c0c589b20
Rick Beerendonk <rick@beerendonk.com>
2019-06-24 07:42:48 +0200

    Add warning about Angular CLI.

--- 02 - Component - c. Styles/3. Relative URL - a. Multiple/app/greeting.component.ts
+++ 02 - Component - c. Styles/3. Relative URL - a. Multiple/app/greeting.component.ts
@@ -4,6 +4,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // moduleId is NOT needed in Angular CLI.
   moduleId: __moduleName /* Use __moduleName in SystemJS and module.id in commonjs */,
   selector: 'greeting',
   styleUrls: ['./greeting.component.css', './second.greeting.component.css'],
