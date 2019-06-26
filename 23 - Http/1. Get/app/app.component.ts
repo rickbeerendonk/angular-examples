e9c77036fe0a0c6816aa0ab6fc73d031adb896a1
Rick Beerendonk <rick@beerendonk.com>
2019-06-26 04:54:05 +0200

    Add "Http - Get" example.

---
+++ 23 - Http/1. Get/app/app.component.ts
@@ -0,0 +1,30 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+class Todo {
+  constructor(
+    public userId: number,
+    public id: number,
+    public title: string,
+    public completed: boolean
+  ) {}
+}
+
+@Component({
+  moduleId: __moduleName /* Use __moduleName in SystemJS and module.id in commonjs */,
+  selector: 'app',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  private baseUrl: string = 'https://jsonplaceholder.typicode.com/todos'; // Free online service
+
+  todos: Observable<Array<Todo>> = this.http.get(this.baseUrl) as Observable<
+    Array<Todo>
+  >;
+
+  constructor(private http: HttpClient) {}
+}
