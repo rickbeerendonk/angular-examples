699b7a0ed36a9b91900762501cc15ee2859115f7
Rick Beerendonk <rick@beerendonk.com>
2019-10-05 13:21:33 +0200

    Add "Component - Template - Inline - Template String" example.

---
+++ 02 - Component - b. Template/1. Inline - a. Template String/app/greeting.component.ts
@@ -0,0 +1,14 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'greeting',
+  template: `
+    <h1>
+      Hello World
+    </h1>
+  `
+})
+export class GreetingComponent {}
