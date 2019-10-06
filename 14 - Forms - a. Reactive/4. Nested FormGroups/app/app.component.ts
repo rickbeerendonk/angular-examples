5c20ed5d35df0917191dc3880efa8fcceaa7b169
Rick Beerendonk <rick@beerendonk.com>
2019-10-06 17:22:18 +0200

    Add "Form - Reactive - Nested FormGroups" example.

---
+++ 14 - Forms - a. Reactive/4. Nested FormGroups/app/app.component.ts
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  template: `
+    <my-form></my-form>
+  `
+})
+export class AppComponent {}
