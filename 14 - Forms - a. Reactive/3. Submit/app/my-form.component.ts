6b12f2514627461afee0f0d497c97c4d52d068fe
Rick Beerendonk <rick@beerendonk.com>
2019-10-06 17:09:17 +0200

    Add "Form - Reactive - Submit" example.

---
+++ 14 - Forms - a. Reactive/3. Submit/app/my-form.component.ts
@@ -0,0 +1,24 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  // moduleId is NOT needed in Angular CLI.
+  moduleId: __moduleName /* Use __moduleName in SystemJS and module.id in commonjs */,
+  selector: 'my-form',
+  templateUrl: './my-form.component.html'
+})
+export class MyFormComponent {
+  personForm = new FormGroup({
+    name: new FormControl('Alexandra'),
+    age: new FormControl(42)
+  });
+
+  submittedData: string;
+
+  submit() {
+    this.submittedData = this.personForm.value;
+  }
+}
