8cd52ab3061ae36f5d2b8e643d989047432aa635
Rick Beerendonk <rick@beerendonk.com>
2019-10-08 13:50:38 +0200

    Name independent of Template.

--- 03 - Data Binding/3. Handling event data/c. Event Filtering/app/greetingeditor.component.ts
+++ 03 - Data Binding/3. Handling event data/c. Event Filtering/app/greetingeditor.component.ts
@@ -6,14 +6,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'greetingeditor',
   template: `
-    <input autofocus #newname (keyup.enter)="onEnter(newname.value)" />
+    <input autofocus #newname (keyup.enter)="onNewName(newname.value)" />
     <h1>Hello {{ name }}!</h1>
   `
 })
 export class GreetingEditorComponent {
   name: string = '';
 
-  onEnter(newName: string) {
+  onNewName(newName: string) {
     this.name = newName;
   }
 }
