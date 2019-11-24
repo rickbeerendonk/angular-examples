681fc3f5f93f53f299b7de8a1859d50d1ee412c9
Rick Beerendonk <rick@beerendonk.com>
2019-11-25 00:47:39 +0100

    Add "Router - Lazy Loading - Preloading" example.

---
+++ 13 - Router/6. Lazy Loading - a. Preloading/default/app/extra/page4/page4.component.ts
@@ -0,0 +1,11 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: __moduleName /* Use __moduleName in SystemJS and module.id in commonjs */,
+  selector: 'page4',
+  templateUrl: './page4.component.html'
+})
+export class Page4Component {}
---
+++ 13 - Router/6. Lazy Loading - a. Preloading/default/app/home/home.component.ts
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: __moduleName /* Use __moduleName in SystemJS and module.id in commonjs */,
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {}
