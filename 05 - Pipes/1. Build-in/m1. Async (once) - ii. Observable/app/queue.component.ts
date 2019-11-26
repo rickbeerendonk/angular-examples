f508f9166f84c581f701c501e0a4dd051cc7460e
Rick Beerendonk <rick@beerendonk.com>
2019-11-26 04:13:01 +0100

    Add "m1. Async (once) - ii. Observable" example.

---
+++ 05 - Pipes/1. Build-in/m1. Async (once) - ii. Observable/app/queue.component.ts
@@ -0,0 +1,20 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+import { from, Observable } from 'rxjs';
+
+@Component({
+  moduleId: __moduleName /* Use __moduleName in SystemJS and module.id in commonjs */,
+  selector: 'queue',
+  templateUrl: './queue.component.html'
+})
+export class QueueComponent {
+  firstArrival: Observable<Date> = from<Promise<Date>>(
+    new Promise(function(resolve /*, reject */) {
+      setTimeout(function() {
+        resolve(new Date());
+      }, 3000);
+    })
+  );
+}
