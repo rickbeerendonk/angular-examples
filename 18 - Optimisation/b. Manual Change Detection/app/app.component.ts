f95c2be2595dff7574a8b688ee08d1bb22d7276e
Rick Beerendonk <rick@beerendonk.com>
2019-11-27 08:23:36 +0100

    Add "Optimisation - Manual Change Detection" example.

---
+++ 18 - Optimisation/b. Manual Change Detection/app/app.component.ts
@@ -0,0 +1,27 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import { Component, ChangeDetectorRef } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  template: `
+    <input autofocus #text (keyup)="(0)" />
+    <h1>App: Hello {{ text.value }}</h1>
+
+    <hr />
+
+    <greeting></greeting>
+  ` // onKeyUp not handled, but Angular will update bindings after event.
+})
+export class AppComponent {
+  constructor(changeDetectorRef: ChangeDetectorRef) {
+    // No automatic change detection
+    changeDetectorRef.detach();
+
+    // Manual change detection
+    setInterval(() => {
+      changeDetectorRef.detectChanges();
+    }, 2000);
+  }
+}
