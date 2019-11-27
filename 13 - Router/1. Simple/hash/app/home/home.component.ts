59fe7ca69eaf2a00ac2f8b6b4e07ab458f9d8117
Rick Beerendonk <rick@beerendonk.com>
2019-11-27 10:21:39 +0100

    Some comment about "unused" code.

--- 13 - Router/1. Simple/hash/app/home/home.component.ts
+++ 13 - Router/1. Simple/hash/app/home/home.component.ts
@@ -5,7 +5,8 @@ import { Component } from '@angular/core';
 
 @Component({
   moduleId: __moduleName /* Use __moduleName in SystemJS and module.id in commonjs */,
-  selector: 'home',
+  selector:
+    'home' /* Selector is not used in this app. Component created through router. */,
   templateUrl: './home.component.html'
 })
 export class HomeComponent {}
--- 13 - Router/1. Simple/hash/app/page2/page2.component.ts
+++ 13 - Router/1. Simple/hash/app/page2/page2.component.ts
@@ -5,7 +5,8 @@ import { Component } from '@angular/core';
 
 @Component({
   moduleId: __moduleName /* Use __moduleName in SystemJS and module.id in commonjs */,
-  selector: 'page2',
+  selector:
+    'page2' /* Selector is not used in this app. Component created through router. */,
   templateUrl: './page2.component.html'
 })
 export class Page2Component {}
