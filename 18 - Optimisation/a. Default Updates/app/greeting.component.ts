50fcf8de1c67ecc438146bd89c9460b0127b4794
Rick Beerendonk <rick@beerendonk.com>
2019-11-27 08:18:37 +0100

    Add "Optimisation - Default Updates" example.

---
+++ 18 - Optimisation/a. Default Updates/app/greeting.component.ts
@@ -0,0 +1,13 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'greeting',
+  template: `
+    <input autofocus #text />
+    <h1>Greeting: Hello {{ text.value }}</h1>
+  ` // onKeyUp not handled, but Angular will update bindings after event.
+})
+export class GreetingComponent {}
