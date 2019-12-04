b8591e4debe2f49d9b85687e8b7cf3b06ac25ee6
Rick Beerendonk <rick@beerendonk.com>
2019-12-04 20:24:06 +0100

    Add "Forms - React - Custom Validator" example.

---
+++ 14 - Forms - a. Reactive/7. Custom Validator/app/my-form.component.ts
@@ -0,0 +1,31 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+import { prefixValidator } from './prefix.directive';
+
+@Component({
+  // moduleId is NOT needed in Angular CLI.
+  moduleId: __moduleName /* Use __moduleName in SystemJS and module.id in commonjs */,
+  selector: 'my-form',
+  styles: ['span { color: gray }'],
+  templateUrl: './my-form.component.html'
+})
+export class MyFormComponent implements OnInit {
+  personForm: FormGroup;
+
+  ngOnInit(): void {
+    this.personForm = new FormGroup({
+      name: new FormControl('', [prefixValidator('Ang')]),
+      age: new FormControl(0)
+    });
+  }
+
+  submittedData: string;
+
+  submitted() {
+    this.submittedData = this.personForm.value;
+  }
+}
