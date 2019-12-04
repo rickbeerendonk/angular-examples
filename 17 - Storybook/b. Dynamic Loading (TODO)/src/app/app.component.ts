6e5e54ecd00df3152561a44db06dec438ee6633f
Rick Beerendonk <rick@beerendonk.com>
2019-12-04 13:23:30 +0100

    Add TODO.

---
+++ 17 - Storybook/b. Dynamic Loading (TODO)/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <greeting name="World"></greeting>
+  `,
+  styles: []
+})
+export class AppComponent {}
---
+++ 17 - Storybook/b. Dynamic Loading (TODO)/src/app/greeting/greeting.component.stories.ts
@@ -0,0 +1,29 @@
+import { storiesOf } from '@storybook/angular';
+import { text } from '@storybook/addon-knobs';
+
+import { GreetingComponent } from './greeting.component';
+
+storiesOf('GreetingComponent', module)
+  .add('without name', () => ({
+    component: GreetingComponent,
+    props: {
+      name: ''
+    },
+    info: {
+      text: `
+            Component won't show "Hello !" when there is no name, but instead doesn't show *anything* at all.
+          `
+    }
+  }))
+  .add('with name', () => ({
+    component: GreetingComponent,
+    props: {
+      name: 'Storybook'
+    }
+  }))
+  .add('knobs', () => ({
+    component: GreetingComponent,
+    props: {
+      name: text('Name', 'Storybook')
+    }
+  }));
