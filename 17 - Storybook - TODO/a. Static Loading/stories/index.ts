f4b04a248d0cdae7ad01dadd2216b96a6a80cfcc
Rick Beerendonk <rick@beerendonk.com>
2020-01-09 14:19:00 +0100

    Add notes to story (just like in the dynamic scenario).

--- 17 - Storybook - TODO/a. Static Loading/stories/index.ts
+++ 17 - Storybook - TODO/a. Static Loading/stories/index.ts
@@ -4,17 +4,19 @@ import { text } from '@storybook/addon-knobs';
 import { GreetingComponent } from '../src/app/greeting/greeting.component';
 
 storiesOf('GreetingComponent', module)
-  .add('without name', () => ({
-    component: GreetingComponent,
-    props: {
-      name: ''
-    },
-    info: {
-      text: `
-            Component won't show "Hello !" when there is no name, but instead doesn't show *anything* at all.
-          `
+  .add(
+    'without name',
+    () => ({
+      component: GreetingComponent,
+      props: {
+        name: ''
+      }
+    }),
+    {
+      notes:
+        'Component won\'t show "Hello !" when there is no name, but instead doesn\'t show anything at all.'
     }
-  }))
+  )
   .add('with name', () => ({
     component: GreetingComponent,
     props: {
