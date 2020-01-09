cd92a0d764e1b13741b904ff5c2ab6b008379add
Rick Beerendonk <rick@beerendonk.com>
2020-01-09 14:06:45 +0100

    Fix issues. Upgrade to Storybook 5.3 rc.

---
+++ 17 - Storybook/a. Static Loading/.storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ['../stories/index.ts'],
+  addons: [
+    '@storybook/addon-actions/register',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-links/register',
+    '@storybook/addon-notes/register'
+  ]
+};
---
+++ 17 - Storybook/b. Dynamic Loading (TODO)/.storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ['../src/**/*.stories.(js|ts)'],
+  addons: [
+    '@storybook/addon-actions/register',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-links/register',
+    '@storybook/addon-notes/register'
+  ]
+};
