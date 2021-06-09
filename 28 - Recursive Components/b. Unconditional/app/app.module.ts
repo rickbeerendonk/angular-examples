d0176a040dfba8a281db2edd540e887e934fb6a8
Rick Beerendonk <rick@beerendonk.com>
2021-06-10 01:56:12 +0200

    Add "Recursive Components - Unconditional" example.

---
+++ 28 - Recursive Components/b. Unconditional/app/app.module.ts
@@ -0,0 +1,15 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { GreetingComponent } from './greeting.component';
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [AppComponent, GreetingComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ 28 - Recursive Components/b. Unconditional/app/greeting.component.ts
@@ -0,0 +1,13 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'greeting',
+  template: `
+    <h1>Hello World</h1>
+    <greeting></greeting>
+  `
+})
+export class GreetingComponent {}
