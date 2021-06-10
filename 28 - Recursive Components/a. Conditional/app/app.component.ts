03c7e7d834e650dc189445ed2be8a2427d3039e1
Rick Beerendonk <rick@beerendonk.com>
2021-06-10 02:09:05 +0200

    Add "Recursive Components - Conditional" example.

---
+++ 28 - Recursive Components/a. Conditional/app/app.component.ts
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  template: ` <greeting></greeting> `
+})
+export class AppComponent {}
