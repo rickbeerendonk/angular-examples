a10ec9945e64c81b8f62618f7970b4f9e29da857
Rick Beerendonk <rick@beerendonk.com>
2021-06-27 12:09:43 +0200

    Add "Data Binding - Miscellaneous - Shorthand Property in Template" example.

---
+++ 03 - Data Binding/5. Miscellaneous/d. Shorthand Property in Template/app/app.component.ts
@@ -0,0 +1,16 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId:
+    __moduleName /* Use __moduleName in SystemJS and module.id in commonjs */,
+  selector: 'app',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  prop1 = 'abc';
+  prop2 = 123;
+  prop3 = true;
+}
