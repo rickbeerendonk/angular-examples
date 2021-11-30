9e25b094ca6e15a5152ff0702a498e38aa002c5b
Rick Beerendonk <rick@beerendonk.com>
2021-11-30 01:55:20 +0100

    Add "Component - Template - Relative URL - SVG" demo.

---
+++ 02 - Component - b. Template/3. Relative URL - b. SVG/app/greeting.component.ts
@@ -0,0 +1,13 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  // moduleId is NOT needed in Angular CLI.
+  moduleId:
+    __moduleName /* Use __moduleName in SystemJS and module.id in commonjs */,
+  selector: 'greeting',
+  templateUrl: './greeting.component.svg'
+})
+export class GreetingComponent {}
