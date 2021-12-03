3675f02e30ef6e30ed9e43f1605b32a38d70b587
Rick Beerendonk <rick@beerendonk.com>
2021-12-03 01:26:28 +0100

    Fix import.

--- 15 - Http/1. Get - a. With Service/app/todos.service.ts
+++ 15 - Http/1. Get - a. With Service/app/todos.service.ts
@@ -3,7 +3,7 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 
 export class Todo {
   constructor(
