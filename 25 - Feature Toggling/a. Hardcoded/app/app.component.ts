99fe0a03558e3d85d3d2f8e25473fd1a59816280
Rick Beerendonk <rick@beerendonk.com>
2022-03-01 10:05:18 +0100

    Add "Feature Toggling - Hardcoded" example.

---
+++ 25 - Feature Toggling/a. Hardcoded/app/app.component.ts
@@ -0,0 +1,15 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+
+import { environment } from '../environments/environment';
+
+@Component({
+  moduleId: __moduleName /* NOT needed in Angular CLI */,
+  selector: 'app',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  feature1 = environment.feature1;
+}
---
+++ 25 - Feature Toggling/a. Hardcoded/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  feature1: true
+};
