21b952e54755bd37ccdca3ea69a164417ccdfa54
Rick Beerendonk <rick@beerendonk.com>
2022-03-01 21:08:11 +0100

    Add comment about why fields need to be public.

--- 03 - Data Binding/1. Data Source to View Target/b2. Property (Punctuation)/app/greeting.component.ts
+++ 03 - Data Binding/1. Data Source to View Target/b2. Property (Punctuation)/app/greeting.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   template: '<h1>Hello <span [innerText]="name"></span>!</h1>'
 })
 export class GreetingComponent {
+  // Don't use private, because it won't work in AOT (ahead of time) compilation.
+  // See: https://angular.io/guide/aot-compiler
   name: string = 'World';
 }
