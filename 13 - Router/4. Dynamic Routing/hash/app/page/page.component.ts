5d2f541ae81982598f326a8974402d4077463242
Rick Beerendonk <rick@beerendonk.com>
2022-03-02 11:02:16 +0100

    Correct type (no any). Use console to show behaviour.

--- 13 - Router/4. Dynamic Routing/hash/app/page/page.component.ts
+++ 13 - Router/4. Dynamic Routing/hash/app/page/page.component.ts
@@ -3,6 +3,7 @@
 
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   moduleId:
@@ -13,14 +14,17 @@ import { ActivatedRoute } from '@angular/router';
 export class PageComponent implements OnInit, OnDestroy {
   id: number;
 
-  subscriberParams: any;
+  subscriberParams: Subscription;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute) {
+    //console.log('constructor');
+  }
 
   ngOnInit() {
     // Is called after the @Input() properties are set.
     // We can do this in the constructor, if we don't use @Input() properties:
     this.subscriberParams = this.route.paramMap.subscribe(paramMap => {
+      //console.log('new id:', paramMap.get('id'));
       this.id = +paramMap.get('id'); // (+) converts string 'id' to a number
     });
   }
