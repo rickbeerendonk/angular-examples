3f8334ad4bf93434d8401177bda6ebfefafd1da7
Rick Beerendonk <rick@beerendonk.com>
2022-03-02 23:42:38 +0100

    Consistent example.

--- 13 - Router/1. Simple/default/app/home/home.component.ts
+++ 13 - Router/1. Simple/default/app/home/home.component.ts
@@ -5,8 +5,7 @@ import { Component } from '@angular/core';
 
 @Component({
   moduleId: __moduleName /* NOT needed in Angular CLI */,
-  selector:
-    'home' /* Selector is not used in this app. Component created through router. */,
+  /* selector: is not needed in this app. Component created through router. */
   templateUrl: './home.component.html'
 })
 export class HomeComponent {}
