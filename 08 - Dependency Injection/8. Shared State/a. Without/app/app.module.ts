03b9378ec514515b189ac5c5c3114db41a9537dc
Rick Beerendonk <rick@beerendonk.com>
2022-03-03 01:45:07 +0100

    Add "Dependency Injection - Shared State - Without" example.

---
+++ 08 - Dependency Injection/8. Shared State/a. Without/app/app.module.ts
@@ -0,0 +1,22 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2022 Rick Beerendonk          !*/
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { TopComponent } from './components/top.component';
+import { MiddleComponent } from './components/middle.component';
+import { BottomComponent } from './components/bottom.component';
+import { SwitchComponent } from './components/switch.component';
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [
+    TopComponent,
+    MiddleComponent,
+    BottomComponent,
+    SwitchComponent
+  ],
+  bootstrap: [TopComponent]
+})
+export class AppModule {}
---
+++ 08 - Dependency Injection/8. Shared State/a. Without/app/components/top.component.ts
@@ -0,0 +1,19 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2022 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+import { reduceEachLeadingCommentRange } from 'typescript';
+
+@Component({
+  selector: 'top',
+  template: `
+    <middle [color]="color"></middle>
+    <switch (switch)="handleSwitch()"></switch>
+  `
+})
+export class TopComponent {
+  color = 'red';
+  handleSwitch() {
+    this.color = this.color === 'red' ? 'green' : 'red';
+  }
+}
