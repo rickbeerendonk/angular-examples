777f78442534116234a983143f0f763a37222355
Rick Beerendonk <rick@beerendonk.com>
2022-03-03 06:29:32 +0100

    Add "Component Interaction - Parent (Template) to Child - Transferring" example. [WIP]

---
+++ 06 - Component Interaction/1. Parent (Template) to Child - d. Transferring/app/important-anchor/important-anchor.component.ts
@@ -0,0 +1,13 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2022 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: __moduleName /* NOT needed in Angular CLI */,
+  selector: 'important-anchor',
+  templateUrl: './important-anchor.component.html'
+})
+export class ImportantAnchorComponent {
+  // WIP
+}
