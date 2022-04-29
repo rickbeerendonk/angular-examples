fcafbaeb14170e543f6d217e562cda7c7ed5eee6
Rick Beerendonk <rick@beerendonk.com>
2022-04-29 06:37:51 +0200

    Comment code.

--- 70 - NgRx/1. Simple - ii. Object/app/state/counter.selectors.ts
+++ 70 - NgRx/1. Simple - ii. Object/app/state/counter.selectors.ts
@@ -1,7 +1,7 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2022 Rick Beerendonk          !*/
 
-import { createSelector, createFeatureSelector } from '@ngrx/store';
+import { /* createSelector, */ createFeatureSelector } from '@ngrx/store';
 
 export interface CounterState {
   count: Number;
