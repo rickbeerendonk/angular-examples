fdfb33a669603493638a75da6601ad93d755e341
Rick Beerendonk <rick@beerendonk.com>
2022-11-15 23:57:24 +0100

    Remove unused import.

--- 08 - Dependency Injection/8. Shared State/b. With/app/components/middle.component.ts
+++ 08 - Dependency Injection/8. Shared State/b. With/app/components/middle.component.ts
@@ -1,7 +1,7 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2022 Rick Beerendonk          !*/
 
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'middle',
