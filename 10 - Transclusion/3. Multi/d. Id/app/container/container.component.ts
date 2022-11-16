5c6c8dc63fa50436b165104e01eebc29ba99ef34
Rick Beerendonk <rick@beerendonk.com>
2022-11-16 14:07:16 +0100

    Add "Transclusion - Multi - Id" example.

---
+++ 10 - Transclusion/3. Multi/d. Id/app/container/container.component.ts
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: __moduleName /* NOT needed in Angular CLI */,
+  selector: 'container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.css']
+})
+export class ContainerComponent {}
