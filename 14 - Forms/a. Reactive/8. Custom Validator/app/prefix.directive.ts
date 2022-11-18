49c7401088d70321aa66759e0221c3685491cc2a
Rick Beerendonk <rick@beerendonk.com>
2022-11-18 19:53:39 +0100

    Use optional chaining. Improved TypeScript types.

--- 14 - Forms/a. Reactive/8. Custom Validator/app/prefix.directive.ts
+++ 14 - Forms/a. Reactive/8. Custom Validator/app/prefix.directive.ts
@@ -1,12 +1,11 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2019 Rick Beerendonk          !*/
 
-import { AbstractControl, ValidatorFn } from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export function prefixValidator(prefix: string): ValidatorFn {
-  return (control: AbstractControl): { [key: string]: any } | null => {
-    const hasPrefix =
-      control.value.startsWith && control.value.startsWith(prefix);
+  return (control: AbstractControl): ValidationErrors | null => {
+    const hasPrefix = Boolean(control.value?.startsWith(prefix));
     return hasPrefix
       ? null
       : { prefix: { required: prefix, value: control.value } };
