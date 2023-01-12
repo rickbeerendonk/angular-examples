226c58d685fbfd5187d5b36a0e9149096e8a3742
Rick Beerendonk <rick@beerendonk.com>
2023-01-13 00:21:50 +0100

    Add "Component - Styles - @import" example.

---
+++ 02 - Component/c. Styles/3. Import/ngmodule/src/app/app.component.ts
@@ -0,0 +1,13 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  template: `
+    <div>App component</div>
+    <greeting></greeting>
+  `
+})
+export class AppComponent {}
---
+++ 02 - Component/c. Styles/3. Import/standalone/src/app/greeting.component.ts
@@ -0,0 +1,20 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+
+import { ChildComponent } from './child.component';
+
+@Component({
+  selector: 'greeting',
+  standalone: true,
+  imports: [ChildComponent],
+  styles: [
+    '@import "./greeting.component.css"'
+  ] /* Style only apply to this component, not the parent nor the child components */,
+  template: `
+    <div>Hello World</div>
+    <child></child>
+  `
+})
+export class GreetingComponent {}
