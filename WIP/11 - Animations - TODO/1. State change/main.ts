cbc08b45528ede0bb732c9680b9d050d85184dff
Rick Beerendonk <rick@beerendonk.com>
2023-01-16 23:46:16 +0100

    Consistent main.ts content.

--- WIP/11 - Animations - TODO/1. State change/main.ts
+++ WIP/11 - Animations - TODO/1. State change/main.ts
@@ -1,6 +1,9 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2017 Rick Beerendonk          !*/
 
+// Manually import (with npm workspaces, zone.js isn't imported automatically)
+import 'zone.js';
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
