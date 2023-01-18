671964f8cc8d7c9a8910e5feb999203208198a9c
Rick Beerendonk <rick@beerendonk.com>
2023-01-18 10:53:20 +0100

    New update script and folder.

--- WIP/update.mjs
+++ WIP/update.mjs
@@ -1,14 +1,20 @@
-import { existsSync, readdirSync, statSync, unlinkSync } from 'fs';
-import { resolve } from 'path';
+import fs, {
+  copyFileSync,
+  existsSync,
+  readdirSync,
+  statSync,
+  unlinkSync
+} from 'fs';
+import path from 'path';
 
-function walk(path) {
+function walk(dir) {
   let results = [];
 
-  const files = readdirSync(path);
+  const files = readdirSync(dir);
 
   if (files.filter(file => file === 'index.html').length === 1) {
     //console.log('push: ', path);
-    results.push(path);
+    results.push(dir);
   } else {
     for (const file of files) {
       //console.log(file);
@@ -23,7 +29,7 @@ function walk(path) {
         break;
       }
 
-      const fullFile = resolve(path, file);
+      const fullFile = path.join(dir, file);
       //console.log(fullFile);
 
       const stats = statSync(fullFile);
@@ -47,11 +53,22 @@ function deleteIfExists(path) {
   }
 }
 
-for (const path of paths) {
-  console.log(path);
+for (const pathBase of paths) {
+  // Adjust folder
+  console.log(pathBase);
 
-  deleteIfExists(resolve(path, 'systemjs.config.js'));
-  deleteIfExists(resolve(path, 'tsconfig.json'));
+  // Delete unneeded files
+  deleteIfExists(path.join(pathBase, 'systemjs.config.js'));
+  deleteIfExists(path.join(pathBase, 'tsconfig.json'));
+
+  // Create new target folder
+  const pathProject = path.join(pathBase, 'ngmodule');
+  const pathSrc = path.join(pathProject, 'src');
+  //fs.mkdirSync(pathSrc);
+
+  // Copy all files recursively
+  fs.cpSync(pathBase, pathSrc, { recursive: true });
+  //copyFolderRecursiveSync(pathBase, 'ngmodule/src');
 }
 
 console.log('Number of folders: ', paths.length);
