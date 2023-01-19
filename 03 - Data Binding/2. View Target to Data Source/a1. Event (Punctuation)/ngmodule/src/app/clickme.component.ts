33555d053fe3746de818a7700010e15db5554548
Rick Beerendonk <rick@beerendonk.com>
2023-01-19 23:17:48 +0100

    Fix.

--- 03 - Data Binding/2. View Target to Data Source/a1. Event (Punctuation)/ngmodule/src/app/clickme.component.ts
+++ 03 - Data Binding/2. View Target to Data Source/a1. Event (Punctuation)/ngmodule/src/app/clickme.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
 export class ClickMeComponent {
   event = { target: 'my target' };
 
-  clicked(event: PointerEvent) {
+  clicked(event: MouseEvent) {
     debugger;
     alert(`Clicked ${event.target}.`);
   }
