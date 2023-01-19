b03892fd2b38eae8108c2ad0aff7193732a37852
Rick Beerendonk <rick@beerendonk.com>
2023-01-19 23:07:43 +0100

    Add "Data Binding - Miscellaneous - Skip Compilation" example.

---
+++ 03 - Data Binding/5. Miscellaneous/e. Skip Compilation/ngmodule/src/app/app.component.ts
@@ -0,0 +1,17 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  template: `
+    <h1>Hello {{'{{name}}'}}!</h1>
+
+    <hr />
+
+    <p>Doesn't work (only with a space)</p>
+    <h1>Hello &#123; &#123;name&#125;&#125;!</h1>
+  `
+})
+export class AppComponent {}
