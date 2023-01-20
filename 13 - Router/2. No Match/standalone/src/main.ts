b3ef6b473bf9b18842b889bf0c0e0f8156f942c9
Rick Beerendonk <rick@beerendonk.com>
2023-01-20 22:16:54 +0100

    Add Standalone version.

---
+++ 13 - Router/2. No Match/standalone/src/main.ts
@@ -0,0 +1,26 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+// Manually import (with npm workspaces, zone.js isn't imported automatically)
+import 'zone.js';
+
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+
+import { AppComponent } from './app/app.component';
+import { HomeComponent } from './app/home/home.component';
+import { Page1Component } from './app/page1/page1.component';
+import { Page2Component } from './app/page2/page2.component';
+import { NoMatchComponent } from './app/nomatch/nomatch.component';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter([
+      { path: '', component: HomeComponent },
+      { path: 'page1', component: Page1Component },
+      { path: 'page2', component: Page2Component },
+      // '**' should be last:
+      { path: '**', component: NoMatchComponent }
+    ])
+  ]
+});
---
+++ 13 - Router/3. Redirect/standalone/src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  /* selector: is not needed in this app. Component created through router. */
+  standalone: true,
+  templateUrl: './home.component.html'
+})
+export class HomeComponent {}
