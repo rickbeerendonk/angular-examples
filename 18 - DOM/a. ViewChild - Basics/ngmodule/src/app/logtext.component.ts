c3395b881a042d4a6d59d60fe9dea7ae9f3f7fb6
Rick Beerendonk <rick@beerendonk.com>
2023-01-25 11:56:21 +0100

    Add AfterViewInit to demonstrate when reference is available.

--- 18 - DOM/a. ViewChild - Basics/ngmodule/src/app/logtext.component.ts
+++ 18 - DOM/a. ViewChild - Basics/ngmodule/src/app/logtext.component.ts
@@ -1,7 +1,7 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2021 Rick Beerendonk          !*/
 
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'logtext',
@@ -10,10 +10,14 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
     <h1 #h1ref></h1>
   `
 })
-export class LogTextComponent {
+export class LogTextComponent implements AfterViewInit {
   @ViewChild('h1ref') h1Ref?: ElementRef;
 
   onInput(target: EventTarget | null) {
     this.h1Ref!.nativeElement.innerText = (target as HTMLInputElement).value;
+  }
+
+  ngAfterViewInit(): void {
+    this.h1Ref!.nativeElement.innerText = 'Initialized';
   }
 }
