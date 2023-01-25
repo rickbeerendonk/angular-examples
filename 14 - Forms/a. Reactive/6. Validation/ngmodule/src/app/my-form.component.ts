64ed1de50e34af3333566375cdfef033cbd62386
Rick Beerendonk <rick@beerendonk.com>
2023-01-26 00:12:08 +0100

    Fix.

--- 14 - Forms/a. Reactive/6. Validation/ngmodule/src/app/my-form.component.ts
+++ 14 - Forms/a. Reactive/6. Validation/ngmodule/src/app/my-form.component.ts
@@ -1,11 +1,10 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2019 Rick Beerendonk          !*/
 
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  // moduleId is NOT needed in Angular CLI.
   selector: 'my-form',
   styles: ['span { color: gray }'],
   templateUrl: './my-form.component.html'
@@ -16,7 +15,7 @@ export class MyFormComponent {
     age: new FormControl(-1, [Validators.min(0)])
   });
 
-  submittedData?: string;
+  submittedData?: Partial<{ name: string | null; age: number | null }>;
 
   submitted() {
     this.submittedData = this.personGroup.value;
