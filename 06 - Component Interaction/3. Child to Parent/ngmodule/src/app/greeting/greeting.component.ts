52019e5582f497dfc994501947ed41f63b7fd5dc
Rick Beerendonk <rick@beerendonk.com>
2023-03-21 11:38:14 +0100

    Simplification.

--- 06 - Component Interaction/3. Child to Parent/ngmodule/src/app/greeting/greeting.component.ts
+++ 06 - Component Interaction/3. Child to Parent/ngmodule/src/app/greeting/greeting.component.ts
@@ -4,16 +4,15 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
-  selector: 'greeting',
+  selector: 'greeting[change]',
   templateUrl: './greeting.component.html',
   styleUrls: ['./greeting.component.css']
 })
 export class GreetingComponent {
-  value: string = 'Hello World!';
   @Output() change = new EventEmitter<string>();
 
   valueChanged(target: EventTarget | null) {
-    this.value = (target as HTMLInputElement).value;
-    this.change.emit(this.value);
+    const value = (target as HTMLInputElement).value;
+    this.change.emit(value);
   }
 }
