c5d992e9ff45249a1f4390361febdcdc09804096
Rick Beerendonk <rick@beerendonk.com>
2023-05-14 07:28:34 +0200

    Add standalone version.

---
+++ 70 - NgRx/1. Simple - i. Number/standalone/src/app/app.component.ts
@@ -0,0 +1,38 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2022 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { increment, decrement } from './state/counter.actions';
+
+@Component({
+  selector: 'app',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <h1>{{ count$ | async }}</h1>
+    <div>
+      <button (click)="increment()">+</button>
+      <button (click)="increment(5)">+5</button>
+      <button (click)="decrement()">-</button>
+    </div>
+  `
+})
+export class AppComponent {
+  count$: Observable<number>;
+
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.select('count');
+  }
+
+  increment(value: number = 1) {
+    this.store.dispatch(increment({ value }));
+  }
+
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+}
---
+++ 70 - NgRx/1. Simple - i. Number/standalone/src/main.ts
@@ -0,0 +1,24 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+// Manually import (with npm workspaces, zone.js isn't imported automatically)
+import 'zone.js';
+
+import { importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { counterReducer } from './app/state/counter.reducer';
+
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      StoreModule.forRoot({ count: counterReducer }),
+      // Must be imported after importing StoreModule
+      StoreDevtoolsModule.instrument()
+    )
+  ]
+});
