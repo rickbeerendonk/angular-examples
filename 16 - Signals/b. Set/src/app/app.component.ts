dc8ddfe57dd291ab2e55b701fd8b9c92498d5347
Rick Beerendonk <rick@beerendonk.com>
2023-09-14 09:35:16 +0200

    Add "Signals - Set" example.

---
+++ 16 - Signals/b. Set/src/app/app.component.ts
@@ -0,0 +1,21 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  standalone: true,
+  template: `
+    <input (input)="onInput($event.target)" [value]="text()" />
+    <h1>Hello {{ text() }}</h1>
+  `
+})
+export class AppComponent {
+  text = signal('World');
+
+  onInput(target: EventTarget | null) {
+    // Set replaces the previous value
+    this.text.set((target as HTMLInputElement).value);
+  }
+}
