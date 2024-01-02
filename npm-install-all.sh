a3b2d46ac97500e65f947e94171ffb60fd64c086
Rick Beerendonk <rick@beerendonk.com>
2024-01-03 00:58:35 +0100

    Faster version.

--- npm-install-all.sh
+++ npm-install-all.sh
@@ -1,1 +1,1 @@
-find . -type d -maxdepth 6 -exec test -e "{}/package.json" ';' -not -path "*node_modules*" -exec bash -c "cd '{}' && npm install" \;
+find . -type d -maxdepth 6 -not -path "*node_modules*" -exec test -e "{}/package.json" ';' -print | xargs -P 10 -I {} bash -c 'cd "{}" && pwd && npm install'
