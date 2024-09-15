bd04cafe036b3bd95c481fb2534f06e7926998b6
Rick Beerendonk <rick@beerendonk.com>
2024-09-15 19:23:11 +0200

    Add "Security - CSS - HTML Content" example.

---
+++ 19 - Security/a. XSS - HTML Content/standalone/src/app/app.component.ts
@@ -0,0 +1,21 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2024 Rick Beerendonk          !*/
+
+// See: https://angular.dev/best-practices/security#sanitization-example
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  standalone: true,
+  template: `
+    <p style="color: gray">See console for sanitizing message</p>
+    <div>
+      Safe (escaped): <span>{{ htmlString }}</span>
+    </div>
+    <div>"Dangerous" (not escaped): <span [innerHTML]="htmlString"></span></div>
+  `
+})
+export class AppComponent {
+  htmlString = '<u onmouseover=alert("Hacked!")>Dangerous</u>';
+}
