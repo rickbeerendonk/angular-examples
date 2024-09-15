b7dbad86843375b38f9e4710aa458de43d610df2
Rick Beerendonk <rick@beerendonk.com>
2024-09-16 00:51:52 +0200

    Add "Data Binding with Blocks - For-Empty" example.

---
+++ 04a - Data Binding with Blocks/2b. For-Empty/standalone/src/main.ts
@@ -0,0 +1,8 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import { bootstrapApplication } from '@angular/platform-browser';
+
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent);
