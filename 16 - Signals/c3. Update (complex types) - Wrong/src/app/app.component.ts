15907597b6f77d7bf87aaeef887a312b7f1be6ed
Rick Beerendonk <rick@beerendonk.com>
2024-09-17 00:57:15 +0200

    Add TODO.

---
+++ 16 - Signals/c3. Update (complex types) - Wrong/src/app/app.component.ts
@@ -0,0 +1,23 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2024 Rick Beerendonk          !*/
+
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  items = signal([1, 2, 3]);
+
+  addItem() {
+    // Don't mutates value in place
+    this.items.update(items => {
+      items[0] = Math.random();
+      return items;
+    });
+  }
+}
