d1e9b681ef9c516ca09bb8ade3fceee383241f44
Rick Beerendonk <rick@beerendonk.com>
2024-09-16 18:55:23 +0200

    Add types.

--- 16 - Signals/d. Computed/src/app/app.component.ts
+++ 16 - Signals/d. Computed/src/app/app.component.ts
@@ -1,7 +1,13 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2023 Rick Beerendonk          !*/
 
-import { Component, computed, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  Signal,
+  signal,
+  WritableSignal
+} from '@angular/core';
 
 @Component({
   selector: 'app',
@@ -13,8 +19,8 @@ import { Component, computed, signal } from '@angular/core';
   `
 })
 export class AppComponent {
-  text = signal('World');
-  uppercaseText = computed(() => this.text().toUpperCase());
+  text: WritableSignal<string> = signal('World');
+  uppercaseText: Signal<string> = computed(() => this.text().toUpperCase());
 
   onInput(target: EventTarget | null) {
     // Set replaces the previous value
