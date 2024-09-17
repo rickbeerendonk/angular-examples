0351fc8dddd00ee3fb82d2fcf18779e784e5c0ce
Rick Beerendonk <rick@beerendonk.com>
2024-09-17 04:09:14 +0200

    Remove unused import.

--- 40 - Optimisation/c3. OnPush Change Detection - Immutable/ngmodule/src/app/app.component.ts
+++ 40 - Optimisation/c3. OnPush Change Detection - Immutable/ngmodule/src/app/app.component.ts
@@ -1,7 +1,7 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2019 Rick Beerendonk          !*/
 
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
