0f68172c104a008c13509febc658c70b02e8234c
Rick Beerendonk <rick@beerendonk.com>
2024-09-18 09:51:14 +0200

    Add optimisation examples.

---
+++ 40 - Optimisation/d1. Filter after Async Pipe/standalone/src/main.ts
@@ -0,0 +1,8 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import { bootstrapApplication } from '@angular/platform-browser';
+
+import { GreetingComponent } from './app/greeting.component';
+
+bootstrapApplication(GreetingComponent);
---
+++ 40 - Optimisation/d2. Filter before Async Pipe (default change detection)/standalone/src/app/greeting.component.ts
@@ -0,0 +1,44 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2024 Rick Beerendonk          !*/
+
+import {
+  AfterContentChecked,
+  AfterViewChecked,
+  ChangeDetectionStrategy,
+  Component
+} from '@angular/core';
+import { distinctUntilChanged, interval, map, Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+
+function getData(): Observable<{ value1: number; value2: number }> {
+  return interval(1000).pipe(
+    map(i => ({
+      value1: Math.floor(i / 3),
+      value2: i
+    }))
+  );
+}
+
+@Component({
+  selector: 'greeting',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.Default, // Default change detection
+  imports: [AsyncPipe],
+  template: `<h1>Hello {{ (value$ | async) ?? 0 }}</h1>`
+})
+export class GreetingComponent
+  implements AfterContentChecked, AfterViewChecked
+{
+  value$ = getData().pipe(
+    /* Just map is not enough */ map(data => data.value1),
+    /* DistinctUntilChanged is needed */ distinctUntilChanged()
+  );
+
+  ngAfterContentChecked(): void {
+    console.count('GreetingComponent.AfterContentChecked');
+  }
+
+  ngAfterViewChecked(): void {
+    console.count('GreetingComponent.AfterViewChecked');
+  }
+}
