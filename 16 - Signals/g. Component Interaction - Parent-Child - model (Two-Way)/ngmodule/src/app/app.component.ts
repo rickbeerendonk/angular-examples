9111f7df7a530751910cfe4e67c3a9992c1cd203
Rick Beerendonk <rick@beerendonk.com>
2024-09-18 04:36:32 +0200

    Add "Signals - Component Interaction - Parent-Child - model (Two-Way)" example.

---
+++ 16 - Signals/g. Component Interaction - Parent-Child - model (Two-Way)/ngmodule/src/app/app.component.ts
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  value = 'Initial value';
+}
