c63dda3a4cbfbb80a17b7df8f8d5fdbb49988126
Rick Beerendonk <rick@beerendonk.com>
2025-12-02 01:18:35 +0100

    Modernize.

--- 02 - Component/b. Template/2. URL - i. HTML/standalone/src/main.ts
+++
@@ -1,8 +0,0 @@
-/*! European Union Public License version 1.2 !*/
-/*! Copyright © 2023 Rick Beerendonk          !*/
-
-import { bootstrapApplication } from '@angular/platform-browser';
-
-import { GreetingComponent } from './app/greeting.component';
-
-bootstrapApplication(GreetingComponent);
