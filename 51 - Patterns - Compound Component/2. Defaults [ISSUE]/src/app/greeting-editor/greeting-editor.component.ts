28622f483bba2dd9984478dfd007669ab1c069ae
Rick Beerendonk <rick@beerendonk.com>
2025-12-04 10:01:12 +0100

    Fix.

--- 51 - Patterns - Compound Component/2. Defaults [ISSUE]/src/app/greeting-editor/greeting-editor.component.ts
+++ 51 - Patterns - Compound Component/2. Defaults [ISSUE]/src/app/greeting-editor/greeting-editor.component.ts
@@ -1,13 +1,13 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2025 Rick Beerendonk          !*/
 
-import { CommonModule, NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { GreetingEditorDefaultComponent } from './greeting-editor-default.component';
 
 @Component({
   selector: 'greeting-editor',
-  imports: [CommonModule, GreetingEditorDefaultComponent, NgIf],
+  imports: [CommonModule, GreetingEditorDefaultComponent],
   templateUrl: './greeting-editor.component.html'
 })
 export class GreetingEditorComponent {
--- 51 - Patterns - Compound Component/99. WIP/ngmodule [WIP]/src/app/app.component.ts
+++ 51 - Patterns - Compound Component/99. WIP/ngmodule [WIP]/src/app/app.component.ts
@@ -2,10 +2,8 @@
 /*! Copyright © 2023 Rick Beerendonk          !*/
 
 import { Component } from '@angular/core';
-import { NgForOf } from '@angular/common';
 
 @Component({
-  imports: [NgForOf],
   selector: 'app',
   templateUrl: './app.component.html'
 })
