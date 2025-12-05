09e8cd6275aa25e8c2cb70e9f92902297f05da85
Rick Beerendonk <rick@beerendonk.com>
2025-12-05 16:44:43 +0100

    Remove unneeded code.

--- 08 - Dependency Injection/10. InjectionToken - Custom Tokens/src/app/data.service.ts
+++ 08 - Dependency Injection/10. InjectionToken - Custom Tokens/src/app/data.service.ts
@@ -1,10 +1,9 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2025 Rick Beerendonk          !*/
 
-import { Injectable, inject } from '@angular/core';
+import { inject } from '@angular/core';
 import { API_ENDPOINT } from './api.token';
 
-@Injectable()
 export class DataService {
   private apiEndpoint = inject(API_ENDPOINT);
 
--- 15 - Http/1. Get - b. Handle in code (with Service)/src/app/todos.service.ts
+++ 15 - Http/1. Get - b. Handle in code (with Service)/src/app/todos.service.ts
@@ -1,7 +1,7 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2025 Rick Beerendonk          !*/
 
-import { Injectable, inject } from '@angular/core';
+import { inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -12,7 +12,6 @@ export interface Todo {
   completed: boolean;
 }
 
-@Injectable()
 export class TodosService {
   private baseUrl = 'https://jsonplaceholder.typicode.com/todos'; // Free online service
   private http = inject(HttpClient);
