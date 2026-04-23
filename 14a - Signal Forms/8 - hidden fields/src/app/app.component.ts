26e212c47e347c99e7efecd70a446b961c4fee21
Rick Beerendonk <rick@beerendonk.com>
2026-04-23 04:19:03 +0200

    Add "Signal Forms - hidden fields" example.

---
+++ 14a - Signal Forms/8 - hidden fields/src/app/app.component.ts
@@ -0,0 +1,27 @@
+/*! Copyright © 2026 Rick Beerendonk !*/
+
+import { Component, signal } from '@angular/core';
+import { form, Field, required, hidden } from '@angular/forms/signals';
+
+@Component({
+  selector: 'app-root',
+  imports: [Field],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  shippingModel = signal({
+    sameAsBilling: true,
+    shippingAddress: ''
+  });
+
+  shippingForm = form(this.shippingModel, path => {
+    // hidden() takes a predicate over the form value.
+    // A hidden field skips validation and is removed from the DOM.
+    hidden(path.shippingAddress, ({ valueOf }) => valueOf(path.sameAsBilling));
+
+    required(path.shippingAddress, {
+      message: 'Shipping address is required'
+    });
+  });
+}
