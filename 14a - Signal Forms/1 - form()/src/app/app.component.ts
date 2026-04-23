ce22671058668e89c77f67a52927bb87a67878a9
Rick Beerendonk <rick@beerendonk.com>
2026-04-23 04:17:00 +0200

    Add "Signal Forms - form()" example.

---
+++ 14a - Signal Forms/1 - form()/src/app/app.component.ts
@@ -0,0 +1,17 @@
+/*! Copyright © 2026 Rick Beerendonk !*/
+
+import { Component, signal } from '@angular/core';
+import { form, Field } from '@angular/forms/signals';
+
+@Component({
+  selector: 'app-root',
+  imports: [Field],
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  // 1. Model is a plain signal of the data shape.
+  loginModel = signal({ email: '', password: '' });
+
+  // 2. form(modelSignal) returns a field tree mirroring the model.
+  loginForm = form(this.loginModel);
+}
