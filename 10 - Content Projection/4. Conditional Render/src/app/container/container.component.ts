a49fdda5085cfd49014fab7f20faf0463024ff31
Rick Beerendonk <rick@beerendonk.com>
2026-04-24 03:50:12 +0200

    Use Angular v21 options.

--- 10 - Content Projection/4. Conditional Render/src/app/container/container.component.ts
+++ 10 - Content Projection/4. Conditional Render/src/app/container/container.component.ts
@@ -2,10 +2,8 @@
 /*! Copyright © 2025 Rick Beerendonk          !*/
 
 import { Component, input } from '@angular/core';
-import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
-  imports: [NgTemplateOutlet],
   selector: 'container',
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.css']
