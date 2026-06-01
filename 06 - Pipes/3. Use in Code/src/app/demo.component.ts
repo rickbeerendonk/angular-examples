20c9daa362955351b2ab45871cea665ee95fbfd3
Rick Beerendonk <rick@beerendonk.com>
2026-06-01 21:41:02 +0200

    Use inject.

--- 06 - Pipes/3. Use in Code/src/app/demo.component.ts
+++ 06 - Pipes/3. Use in Code/src/app/demo.component.ts
@@ -1,7 +1,7 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2025 Rick Beerendonk          !*/
 
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { UpperCasePipe, DatePipe } from '@angular/common';
 
 @Component({
@@ -10,6 +10,9 @@ import { UpperCasePipe, DatePipe } from '@angular/common';
   providers: [UpperCasePipe, DatePipe]
 })
 export class DemoComponent {
+  private readonly upperCasePipe = inject(UpperCasePipe);
+  private readonly datePipe = inject(DatePipe);
+
   person = {
     name: 'Jan Jansen',
     dob: new Date(1951, 11, 24),
@@ -18,11 +21,6 @@ export class DemoComponent {
   };
 
   // Transformed
-  nameUpperCase?: string;
-  dobString: string | null;
-
-  constructor(upperCasePipe: UpperCasePipe, datePipe: DatePipe) {
-    this.nameUpperCase = upperCasePipe.transform(this.person.name);
-    this.dobString = datePipe.transform(this.person.dob, 'dd-MM-yyyy');
-  }
+  nameUpperCase = this.upperCasePipe.transform(this.person.name);
+  dobString = this.datePipe.transform(this.person.dob, 'dd-MM-yyyy');
 }
