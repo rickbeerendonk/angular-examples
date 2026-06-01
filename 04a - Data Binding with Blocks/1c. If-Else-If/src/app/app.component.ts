66c462d5fed8e5568b680d8c31ec5bbac42a65c6
Rick Beerendonk <rick@beerendonk.com>
2026-06-01 21:40:25 +0200

    Add "Data Binding with Blocks - If-Else-If" example.

---
+++ 04a - Data Binding with Blocks/1c. If-Else-If/src/app/app.component.ts
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  temperature = signal(-2);
+}
