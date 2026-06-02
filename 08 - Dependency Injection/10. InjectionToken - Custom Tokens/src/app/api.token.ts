87849bdaeef76c326e05fa0b538e7f0b447f55a0
Rick Beerendonk <rick@beerendonk.com>
2026-06-02 07:30:34 +0200

    Add comment.

--- 08 - Dependency Injection/10. InjectionToken - Custom Tokens/src/app/api.token.ts
+++ 08 - Dependency Injection/10. InjectionToken - Custom Tokens/src/app/api.token.ts
@@ -3,4 +3,6 @@
 
 import { InjectionToken } from '@angular/core';
 
+// Create a custom injection token for the API endpoint
+// This token can be used to inject the API endpoint string into services or components
 export const API_ENDPOINT = new InjectionToken<string>('apiEndpoint');
