340b8a5835d9d005e45ec1dd424cb8d810312da8
Rick Beerendonk <rick@beerendonk.com>
2026-06-03 14:32:55 +0200

    Add long name.

--- 08 - Dependency Injection/3. Providers in own Injector/src/app/greeting/greeting.component.ts
+++ 08 - Dependency Injection/3. Providers in own Injector/src/app/greeting/greeting.component.ts
@@ -8,6 +8,7 @@ import { LoggerService } from '../services/logger.service';
 @Component({
   selector: 'greeting',
   template: '<h1>Hello World</h1>',
+  //providers: [{ provide: LoggerService, useClass: LoggerService }]
   providers: [LoggerService]
 })
 export class GreetingComponent {
